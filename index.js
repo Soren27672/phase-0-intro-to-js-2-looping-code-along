1906bac7fd4c163e89f367a71e9e6b786f07ed1d
Maxwell Gottesman <maxgman16@yahoo.com>
2022-10-18 17:26:41 -0400

    commit tree 0cb8ce66462f36eb916a7b5bf5378412303cdc5c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array) {
+    let returnArray = [];
+    for (let i = 0; i < array.length; i++) {
+        returnArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return returnArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        --number;
+    }
+}
